fbd3d5702efb5a54cbfce26724c26445968e02e5
steve higgs <stevejhiggs@hotmail.com>
2016-02-14 15:39:42 +0000

    now actually works

--- specs/twitter/homepage.js
+++ specs/twitter/homepage.js
@@ -1,7 +1,9 @@
 describe('twitter homepage', () => {
   it('should load', () => {
     browser.url('https://www.twitter.com');
+    // browser.waitForExist('h2.StreamsHero-header');
+
     var headingValue = browser.getText('h2.StreamsHero-header');
-    headingValue.value.should.be.equal('See what’s happening right now.');
+    headingValue.should.be.equal('See what’s happening right now.');
   });
 });
